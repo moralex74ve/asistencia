9f01ca813de7c7387488127e7081c6313f3f7dbd
moralex74ve <moralex74@gmail.com>
2025-09-09 15:18:59 -0400

    Actualiza configuración y corrige errores de build

--- astro.config.backup.mjs
+++ astro.config.backup.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
