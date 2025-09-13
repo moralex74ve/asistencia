6561717093572f212b99cf155e8f89117ea1e382
moralex74ve <moralex74@gmail.com>
2025-09-12 22:58:37 -0400

    mejoras y previene salir de eventos si esta marcado

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tu Sitio Web';
-export const SITE_DESCRIPTION = 'Frontend Developer. JavaScript Specialist';
+export const SITE_TITLE = 'Control de Asistencia';
+export const SITE_DESCRIPTION = 'App para el control de asistencia de empleados.';
